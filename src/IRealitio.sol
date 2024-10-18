8f9448373f4270f519e4ae4eea36c43483646f0d
Ferit <0xferittuncer@gmail.com>
2024-10-18 18:15:04 +0300

    feat: oracle contracts

---
+++ src/IRealitio.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.8.0;
+
+interface IRealitio {
+    function getContentHash(bytes32 questionId) external view returns (bytes32);
+    function getOpeningTS(bytes32 questionId) external view returns (uint32);
+    function resultFor(bytes32 questionId) external view returns (bytes32);
+}
